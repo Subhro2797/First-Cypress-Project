7e8a326fed0a196b21f13d2b20de4482cb2516a9
Subhro <subhro271197@gmail.com>
2025-04-24 13:58:52 +0600

    Added the Allure Report generation

--- cypress.config.js
+++ cypress.config.js
@@ -1,21 +1,29 @@
-const { defineConfig } = require("cypress");
+const {allureCypress} = require("allure-cypress/reporter");
+const {defineConfig} = require("cypress");
+
+
+
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      allureCypress(on, config, {
+        resultsdir: "allure-results",
+      });
+      return config;
     },
     screenshotOnRunFailure: true,
     video: true, 
 
   },
-  reporter: 'mochawesome',
-  reporterOptions: {
-    reportDir: 'cypress/reports/mochawesome',
-    overwrite: false,
-    html: true, // make sure this is set to true if you want an HTML report
-    json: true,
-}
+//   reporter: 'mochawesome',
+//   reporterOptions: {
+//     reportDir: 'cypress/reports/mochawesome',
+//     overwrite: false,
+//     html: true, // make sure this is set to true if you want an HTML report
+//     json: true,
+// }
 
 
 });
